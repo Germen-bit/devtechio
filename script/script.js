94961180227c9dc56da9e7678673f5a78410f55a
GermenTI <jgermen14@gmail.com>
2021-10-19 22:15:09 +0100

    Second commit

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
 const card = document.querySelectorAll('.card')
 const more = document.querySelectorAll('.more')
+const detailImage = document.querySelector('.detail-image')
+const images = document.querySelectorAll('.images')
 
+/** Codigo para criar o efeito de overlay dos cards da secção team  */
 for(let i = 0; i < card.length; i++){
   card[i].addEventListener('pointerover', ()=> {
     more[i].classList.add('active')
@@ -10,5 +13,12 @@ for(let i = 0; i < card.length; i++){
 for(let i = 0; i < card.length; i++){
   card[i].addEventListener('pointerout', ()=> {
     more[i].classList.remove('active')
+  })
+}
+
+/**  Codigo para alterar as images na secção content */
+for (let i = 0; i < images.length; i ++) {
+  images[i].addEventListener('click', () => {
+    detailImage.setAttribute('src', images[i].getAttribute('src'))
   })
 }
